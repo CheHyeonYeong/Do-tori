3b70a5771239da563d8727753b68c287a73a4445
unknown <gusdudco6@naver.com>
2024-05-08 17:16:31 +0900

    🎉 Congraturation for Begin a project
    Congraturation for Begin a project

---
+++ src/test/java/com/dotori/dotori/DotoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotori.dotori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
